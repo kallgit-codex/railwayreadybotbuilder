5eabc836d47b105a9517367c29b35676a6bdcceb
kallgit-codex <kallistiu@gmail.com>
2025-08-02 02:31:06 -0500

    Delete BotBuilder directory

--- BotBuilder/services/platform_service.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-def register_webhooks(client, bot_id, public_url):
-    # Telegram
-    if client.telegram_api_key:
-        telegram_url = f"https://api.telegram.org/bot{client.telegram_api_key}/setWebhook"
-        requests.post(telegram_url, data={"url": f"{public_url}/webhook/telegram/{bot_id}"})
-    # WhatsApp / Meta would go here with Graph API calls
